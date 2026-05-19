dcdd1185ed27d45ed3362ddc3a22da7311a6c2ac
DPatel1416 <Dhruvkumarpatel07@gmail.com>
2026-05-19 14:01:52 -0400

    conflict-demo

--- SEG_Lab1/src/HelloWorld.java
+++ SEG_Lab1/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String [] args) {
-		System.out.println("Welcome to conflict-demo !");
+		System.out.println("Welcome to branch conflict-demo !");
 	}
 }
