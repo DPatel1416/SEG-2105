65a250512267d90e1fa927027791604f48528246
DPatel1416 <Dhruvkumarpatel07@gmail.com>
2026-05-19 14:04:33 -0400

    Different message

--- SEG_Lab1/src/HelloWorld.java
+++ SEG_Lab1/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String [] args) {
-		System.out.println("Welcome to Hello World.java !");
+		System.out.println("Welcome to Hello World.java, we are CS student !");
 	}
 }
